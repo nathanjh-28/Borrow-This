588eb04a399a7b6d2df31ed614d20d27c2a03ca2
nathanjh-28 <nathan@nathanjharris.com>
2020-08-21 13:31:52 -0600

    added routes for dashboard and browse with placeholder text templates

--- rentals_app/urls.py
+++ rentals_app/urls.py
@@ -2,5 +2,44 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
+    #--------------------------------  Home
+    
     path('',views.home, name='home'),
+
+    #--------------------------------  SignUp Form
+
+
+    #--------------------------------  Login
+
+    
+    #--------------------------------  Dashboard
+    
+    path('dashboard/', views.dashboard, name='dashboard'),
+
+    #--------------------------------  Browse
+    
+    path('browse/', views.browse, name='browse'),
+
+    #--------------------------------  User Public
+
+
+    #--------------------------------  Item Details
+
+
+    #--------------------------------  Add Item Form
+
+
+    #--------------------------------  Add Reservation Form
+
+
+    #--------------------------------  Reservation Details
+
+
+
+    # TBD Update Res, Update Item, Update profile
+    # TBD page for past reservations rather than on the dashboard
+
+
+
 ]
--- rentals_app/views.py
+++ rentals_app/views.py
@@ -2,7 +2,39 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-# Create your views here.
 
+
+#--------------------------------  Home
 def home(request):
     return render(request,'home.html')
+
+
+#--------------------------------  SignUp Form
+
+
+#--------------------------------  Login
+
+
+#--------------------------------  Dashboard
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+#--------------------------------  Browse
+
+def browse(request):
+    return render(request, 'browse.html')
+
+#--------------------------------  User Public
+
+
+#--------------------------------  Item Details
+
+
+#--------------------------------  Add Item Form
+
+
+#--------------------------------  Add Reservation Form
+
+
+#--------------------------------  Reservation Details
