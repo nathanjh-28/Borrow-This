139e3496d3678537171e164e2e42adbbb5ca7fd4
nathanjh-28 <nathan@nathanjharris.com>
2020-08-25 16:59:07 -0600

    got bulma calendar pop up working ish

--- rentals_app/static/js/app.js
+++ rentals_app/static/js/app.js
@@ -1,1 +1,31 @@
 console.log('borrow me!')
+
+// import bulmaCalendar from '../bulma-calendar/dist/js/bulma-calendar.min.js'
+
+// Initialize all input of date type.
+const calendars = bulmaCalendar.attach('[type="date"]');
+const dateTimeCals = bulmaCalendar.attach('[type="datetime-local"]');
+
+// Loop on each calendar initialized
+calendars.forEach(calendar => {
+	// Add listener to date:selected event
+	calendar.on('date:selected', date => {
+		console.log(date);
+	});
+});
+
+dateTimeCals.forEach(cal => {
+    cal.on('datetime-local:selected', date =>{
+        console.log(date)
+    })
+})
+
+// To access to bulmaCalendar instance of an element
+const element = document.querySelector('#my-element');
+if (element) {
+	// bulmaCalendar instance is available as element.bulmaCalendar
+	element.bulmaCalendar.on('select', datepicker => {
+		console.log(datepicker.data.value());
+	});
+}
+
