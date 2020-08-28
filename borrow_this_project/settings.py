2548f555babcd4e5e6eeafab3caf801859f49e9b
nathanjh-28 <nathan@nathanjharris.com>
2020-08-28 01:40:23 -0600

    added some stuff to settings

--- borrow_this_project/settings.py
+++ borrow_this_project/settings.py
@@ -18,8 +18,20 @@ import django_on_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
+# https://devcenter.heroku.com/articles/django-assets
+
+# adding stuff here
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
@@ -54,7 +66,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # stuff added here
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
+
+# stuff added here
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 ROOT_URLCONF = 'borrow_this_project.urls'
 
